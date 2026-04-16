d3e91d4d1fa6b278cfbe8a63e4ee1f2b563813fb
daffa akhlaric <daffa.akhlaric@qonex.id>
2026-04-17 00:06:00 +0700

    fix session dan filter profit

--- utils/index.js
+++ utils/index.js
@@ -4,26 +4,14 @@
  * UTILS MODULE — Helper functions & utilities
  */
 
-const helpers = require("./helpers");
-const logger = require("./logger");
 const time = require("./time");
-const validation = require("./validation");
-const marketRegimeDetector = require("./marketRegimeDetector");
 const tpCalculator = require("./tpCalculator");
 
 module.exports = {
-  helpers,
-  logger,
   time,
-  validation,
-  marketRegimeDetector,
   tpCalculator,
 
   // Direct exports
-  ...helpers,
-  ...logger,
   ...time,
-  ...validation,
-  ...marketRegimeDetector,
   ...tpCalculator,
 };
